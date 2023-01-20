abc19b0d0537124487fffceec3772e8237a6b72d
Ukrainiangirl <60987815+Ukrainiangirl@users.noreply.github.com>
2023-01-20 11:13:42 +0200

    testes

--- Buildforcheck/Program.cs
+++ Buildforcheck/Program.cs
@@ -8,28 +8,29 @@ namespace Buildforcheck
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int c = Sum(2, 3);
-            if (c == 0)
-            {
-                c = 10;
-            }
+            //int c = Sum(2, 3);
+            //if (c == 0)
+            //{
+            //    c = 10;
+            //}
 
-            int d = Minus(10, 0);
-            Console.WriteLine(c);
+            //int d = Minus(10, 0);
+            //Console.WriteLine(c);
         }
 
-        public static int Sum(int a, int b)
+        public int Sum(int a, int b)
         {
             if (a > 3)
                 a = 4;
                 return a + b;
         }
 
-        public static int Minus(int a, int b)
+        public int Minus(int a, int b)
         {
             if (a > 3)
                 a = 4;
             return a - b;
         }
     }
+
 }
--- BuildforcheckTests/ProgramTests.cs
+++ BuildforcheckTests/ProgramTests.cs
@@ -8,13 +8,28 @@ namespace Buildforcheck.Tests
         [TestMethod()]
         public void SumTest()
         {
-            Assert.Fail();
+            // Arrange
+            var t = new Program();
+
+            // Act
+            var actual = t.Sum(1, 2); ;
+
+            // Assert
+            Assert.AreEqual(actual, 3);
         }
+
 
         [TestMethod()]
         public void MinusTest()
         {
-            Assert.Fail();
+            // Arrange
+            var t = new Program();
+
+            // Act
+            var actual = t.Minus(4, 2); ;
+
+            // Assert
+            Assert.AreEqual(actual, 2);
         }
     }
 }
