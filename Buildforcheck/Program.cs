144624870e16252647566beacd2383e3f0a360e2
Ukrainiangirl <60987815+Ukrainiangirl@users.noreply.github.com>
2023-01-20 10:17:49 +0200

    Add project files.

---
+++ Buildforcheck/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Buildforcheck
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int c = Sum(2, 3);
+            if (c == 0)
+            {
+                c = 10;
+            }
+
+            int d = Minus(0, 0);
+            Console.WriteLine(c);
+        }
+
+        public static int Sum(int a, int b)
+        {
+            if (a > 3)
+                a = 4;
+                return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            if (a > 3)
+                a = 4;
+            return a - b;
+        }
+    }
+}
---
+++ BuildforcheckTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Buildforcheck.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MinusTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
